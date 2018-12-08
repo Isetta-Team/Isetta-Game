670f9292912fde2924e6ec728340135f2788fea2
Chaojie Zhu <zackspbc@gmail.com>
2018-12-07 20:22:34 -0500

    Add obstacle

--- TwinStickGame/TwinStickGame/TestComponent.h
+++ TwinStickGame/TwinStickGame/TestComponent.h
@@ -14,7 +14,25 @@ void Update() override;
 Nav2DAgent* agent;
 
 Nav2DPlane plane;
-TestComponent() : plane{Math::Rect{-75, -25, 100, 100}, Math::Vector2Int{75, 75}} {
+TestComponent()
+    : plane{Math::Rect{-75, -25, 100, 100}, Math::Vector2Int{75, 75}} {
+  plane.AddObstacle(Nav2DObstacle({{-53.f, -7.f},
+                                   {-1.f, -7.f},
+                                   {-1, 12},
+                                   {17, 12},
+                                   {17, 23},
+                                   {-1, 23},
+                                   {-1, 41},
+                                   {-25, 41},
+                                   {-25, 59},
+                                   {-47, 59},
+                                   {-47, 48},
+                                   {-48, 48},
+                                   {-48, 47},
+                                   {-65, 47},
+                                   {-65, 30},
+                                   {-53, 30},
+                                   {-53, -7}}));
   // plane.AddObstacle(Nav2DObstacle{Array<Math::Vector2>{
 
   //}});
